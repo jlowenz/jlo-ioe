f1b630f786bf0de5b39d16d3423ff59a0aba7fcf
jlowens <jlowens@80c2bdf0-262c-0410-a855-035c9059e91e>
2007-04-18 04:24:42 +0000

    get sshiny to compile. new version of the keynote

--- trunk/sshiny/src/jlo/ioe/Indexer.scala
+++ trunk/sshiny/src/jlo/ioe/Indexer.scala
@@ -24,7 +24,7 @@ import java.io.File
 import java.io.FileNotFoundException
 import java.io.IOException
 import java.util.Date
-import jlo.ioe.data
+import jlo.ioe.data._
 
 object IndexObject {
   val INDEX_DIR = new File("db/index");
@@ -34,60 +34,46 @@ object IndexObject {
   }
   
   /** Index all text files under a directory. */
-  public static void main(String[] args) {
+  def main(args : Array[String]) = {
     if (INDEX_DIR.exists()) {
       System.out.println("Cannot save index to '" +INDEX_DIR+ "' directory, please delete it first");
       System.exit(1);
     }
     
-    final File docDir = new File(args[0]);
+    val docDir = new File(args(0));
     if (!docDir.exists() || !docDir.canRead()) {
       System.out.println("Document directory '" +docDir.getAbsolutePath()+ "' does not exist or is not readable, please check the path");
       System.exit(1);
     }
     
-    Date start = new Date();
-    try {
-      IndexWriter writer = new IndexWriter(INDEX_DIR, new StandardAnalyzer(), true);
-      System.out.println("Indexing to directory '" +INDEX_DIR+ "'...");
-      indexDocs(writer, docDir);
-      System.out.println("Optimizing...");
-      writer.optimize();
-      writer.close();
+    val start = new Date();
+    val writer = new IndexWriter(INDEX_DIR, new StandardAnalyzer(), true);
+    System.out.println("Indexing to directory '" +INDEX_DIR+ "'...");
+    indexDocs(writer, docDir);
+    System.out.println("Optimizing...");
+    writer.optimize();
+    writer.close();
+    
+    val end = new Date();
+    System.out.println(end.getTime() - start.getTime() + " total milliseconds");
 
-      Date end = new Date();
-      System.out.println(end.getTime() - start.getTime() + " total milliseconds");
-
-    } catch (IOException e) {
-      System.out.println(" caught a " + e.getClass() +
-       "\n with message: " + e.getMessage());
-    }
   }
 
-  static void indexDocs(IndexWriter writer, File file)
-    throws IOException {
+  def indexDocs(writer: IndexWriter, file : File) : Unit = {
     // do not try to index files that cannot be read
     if (file.canRead()) {
       if (file.isDirectory()) {
-        String[] files = file.list();
+        val files = file.list();
         // an IO error could occur
         if (files != null) {
-          for (int i = 0; i < files.length; i++) {
-            indexDocs(writer, new File(file, files[i]));
+          for (val f <- files) {
+            indexDocs(writer, new File(file, f));
           }
         }
       } else {
         System.out.println("adding " + file);
-        try {
-          writer.addDocument(FileDocument.Document(file));
-        }
-        // at least on windows, some temporary files raise this exception with an "access denied" message
-        // checking if the file can be read doesn't help
-        catch (FileNotFoundException fnfe) {
-          ;
-        }
+        //writer.addDocument(FileDocument.Document(file));
       }
     }
   }
-  
 }
